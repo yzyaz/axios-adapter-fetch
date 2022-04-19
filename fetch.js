c31d81eec2e2ff36899e57f71786260def225abf
yuzhiyong <yuzhiyong@newrank.cn>
2022-04-19 09:46:14 +0800

    部分优化

--- fetch.js
+++ fetch.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var utils = require('axios/lib/utils');
-var settle = require('axios/lib/core/settle');
-var cookies = require('axios/lib/helpers/cookies');
-var buildURL = require('axios/lib/helpers/buildURL');
-var buildFullPath = require('axios/lib/core/buildFullPath');
-var isURLSameOrigin = require('axios/lib/helpers/isURLSameOrigin');
-var createError = require('axios/lib/core/createError');
+const utils = require('axios/lib/utils');
+const settle = require('axios/lib/core/settle');
+const cookies = require('axios/lib/helpers/cookies');
+const buildURL = require('axios/lib/helpers/buildURL');
+const buildFullPath = require('axios/lib/core/buildFullPath');
+const isURLSameOrigin = require('axios/lib/helpers/isURLSameOrigin');
+const createError = require('axios/lib/core/createError');
 
 module.exports = function xhrAdapter(config) {
   return new Promise(function dispatchXhrRequest(resolve, reject) {
@@ -16,11 +16,11 @@ module.exports = function xhrAdapter(config) {
     // - string, plain object, ArrayBuffer, ArrayBufferView, URLSearchParams
     // - 浏览器专属：FormData, File, Blob
     // - Node 专属： Stream
-    var requestData = config.data;
+    let requestData = config.data;
     // `headers` 是即将被发送的自定义请求头
-    var requestHeaders = config.headers;
+    let requestHeaders = config.headers;
     // `responseType` 表示服务器响应的数据类型，可以是 'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'
-    var responseType = config.responseType;
+    let responseType = config.responseType;
     // 是否正在请求中
     let reqing = false;
     // 取消请求相关
@@ -32,10 +32,10 @@ module.exports = function xhrAdapter(config) {
       delete requestHeaders['Content-Type']; // Let the browser set it
     }
 
-    // HTTP basic authentication
+    // HTTP基本身份验证
     if (config.auth) {
-      var username = config.auth.username || '';
-      var password = config.auth.password
+      let username = config.auth.username || '';
+      let password = config.auth.password
         ? unescape(encodeURIComponent(config.auth.password))
         : '';
       requestHeaders.Authorization = 'Basic ' + btoa(username + ':' + password);
@@ -47,12 +47,12 @@ module.exports = function xhrAdapter(config) {
         typeof requestData === 'undefined' &&
         key.toLowerCase() === 'content-type'
       ) {
-        // Remove Content-Type if data is undefined
+        // 如果数据undefined，删除Content-Type
         delete requestHeaders[key];
       }
     });
 
-    var fullPath = buildFullPath(config.baseURL, config.url);
+    let fullPath = buildFullPath(config.baseURL, config.url);
 
     // `xsrfHeaderName` is the name of the http header that carries the xsrf token value
     // Add xsrf header
@@ -60,7 +60,7 @@ module.exports = function xhrAdapter(config) {
     // Specifically not if we're in a web worker, or react-native.
     if (utils.isStandardBrowserEnv()) {
       // Add xsrf header
-      var xsrfValue =
+      let xsrfValue =
         (config.withCredentials || isURLSameOrigin(fullPath)) &&
         config.xsrfCookieName
           ? cookies.read(config.xsrfCookieName)
@@ -73,7 +73,7 @@ module.exports = function xhrAdapter(config) {
 
     // 组合fetch的请求参数
     const method = config.method.toUpperCase();
-    var fetchOptions = {
+    let fetchOptions = {
       method,
       headers: requestHeaders,
     };
@@ -87,15 +87,6 @@ module.exports = function xhrAdapter(config) {
     if (!utils.isUndefined(config.withCredentials)) {
       fetchOptions.credentials = config.withCredentials ? 'include' : 'omit';
     }
-    // // 下载进度
-    // if (typeof config.onDownloadProgress === 'function') {
-    //   config.onDownloadProgress;
-    // }
-
-    // // 上传进度
-    // if (typeof config.onUploadProgress === 'function' && fetchOptions.upload) {
-    //   fetchOptions.upload.addEventListener('progress', config.onUploadProgress);
-    // }
 
     // fetch接口的独特配置, 详见https://developer.mozilla.org/zh-CN/docs/Web/API/fetch
     if (config.mode) {
@@ -119,18 +110,22 @@ module.exports = function xhrAdapter(config) {
       controller = new AbortController();
       signal = controller.signal;
       fetchOptions.signal = signal;
+
+      // 取消请求
+      config.cancelToken.promise.then(function onCanceled(cancel) {
+        if (!reqing) {
+          return;
+        }
+        controller.abort();
+        reject(cancel);
+      });
     }
 
     // 上传进度暂不支持
     // const onUploadProgress = async (fetchReq) => {
     //   if (typeof config.onUploadProgress !== 'function') return;
-    //   // Step 1: start the fetch and obtain a reader
     //   const reader = fetchReq.clone().body.getReader(); // 这里body为null,走不通
-
-    //   // Step 2: get total length
     //   const contentLength = +fetchReq.headers.get('Content-Length');
-
-    //   // Step 3: read the data
     //   let receivedLength = 0; // received that many bytes at the moment
     //   let chunks = []; // array of received binary chunks (comprises the body)
     //   while (true) {
@@ -148,16 +143,10 @@ module.exports = function xhrAdapter(config) {
     // 下载进度
     const onDownloadProgress = async (fetchRes) => {
       if (typeof config.onDownloadProgress !== 'function') return;
-
-      // Step 1: start the fetch and obtain a reader
       const reader = fetchRes.clone().body.getReader();
-
-      // Step 2: get total length
       const contentLength = +fetchRes.headers.get('Content-Length');
-
-      // Step 3: read the data
-      let receivedLength = 0; // received that many bytes at the moment
-      let chunks = []; // array of received binary chunks (comprises the body)
+      let receivedLength = 0;
+      let chunks = [];
       while (true) {
         const { done, value } = await reader.read();
         if (done) break;
@@ -170,16 +159,36 @@ module.exports = function xhrAdapter(config) {
       }
     };
 
+    // 超时方法
+    const timeoutFun = function () {
+      if (config.timeout && config.timeout > 0) {
+        setTimeout(() => {
+          let timeoutErrorMessage = config.timeout
+            ? 'timeout of ' + config.timeout + 'ms exceeded'
+            : 'timeout exceeded';
+          let transitional = config.transitional || {};
+          if (config.timeoutErrorMessage) {
+            timeoutErrorMessage = config.timeoutErrorMessage;
+          }
+          // 取消请求
+          controller.abort();
+          // 超时返回错误
+          resolve(
+            createError(
+              timeoutErrorMessage,
+              config,
+              transitional.clarifyTimeoutError ? 'ETIMEDOUT' : 'ECONNABORTED',
+              null
+            )
+          );
+        }, config.timeout);
+      }
+    };
+
     // fetch请求接口函数
     async function fetchFun(url, init) {
+      timeoutFun();
       let fetchRes;
-      let response = {
-        status: null,
-        statusText: null,
-        headers: null,
-        config: config,
-        request: null,
-      };
       reqing = true;
       const paramsRequest = new Request(url, init);
       // 上传进度暂不支持
@@ -196,7 +205,7 @@ module.exports = function xhrAdapter(config) {
       // 下载进度
       onDownloadProgress(fetchRes);
 
-      response = {
+      let response = {
         status: fetchRes.status,
         statusText: fetchRes.statusText,
         headers: Object.fromEntries(fetchRes.headers),
@@ -227,53 +236,9 @@ module.exports = function xhrAdapter(config) {
       settle(resolve, reject, response);
     }
 
-    // 取消请求
-    if (config.cancelToken) {
-      config.cancelToken.promise.then(function onCanceled(cancel) {
-        if (!reqing) {
-          return;
-        }
-        controller.abort();
-        reject(cancel);
-      });
-    }
-
-    // promise链,用Promise.race执行返回先执行那个
-    let promiseChain = [
-      fetchFun(
-        buildURL(fullPath, config.params, config.paramsSerializer),
-        fetchOptions
-      ),
-    ];
-
-    // 超时处理
-    if (config.timeout && config.timeout > 0) {
-      promiseChain.push(
-        setTimeout(() => {
-          var timeoutErrorMessage = config.timeout
-            ? 'timeout of ' + config.timeout + 'ms exceeded'
-            : 'timeout exceeded';
-          var transitional = config.transitional || {};
-          if (config.timeoutErrorMessage) {
-            timeoutErrorMessage = config.timeoutErrorMessage;
-          }
-          // 取消请求
-          controller.abort();
-          // 超时返回错误
-          resolve(
-            createError(
-              timeoutErrorMessage,
-              config,
-              transitional.clarifyTimeoutError ? 'ETIMEDOUT' : 'ECONNABORTED',
-              null
-            )
-          );
-        }, config.timeout)
-      );
-    }
-
-    try {
-      Promise.race(promiseChain);
-    } catch (error) {}
+    fetchFun(
+      buildURL(fullPath, config.params, config.paramsSerializer),
+      fetchOptions
+    );
   });
 };
